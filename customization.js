f52d4844dbd9cffdf05f9a2068521d79f17a12f2
Miguel Mesquita <mm@cob.pt>
2022-12-06 16:05:22 +0000

    chore: update version

--- customization.js
+++ customization.js
@@ -1,18 +1,3 @@
 module.exports = {
-  version: "1.0.0",
-
-  questions: [
-    {
-      type: "input",
-      name: "name",
-      message: "What's the name of the definition?",
-      description: "the name of the definition listing to customize",
-    },
-  ],
-
-  actions: async function (repoName, answers, copy, mergeFiles) {
-    const path = require("path");
-    await copy(path.resolve(repoName), "./");
-    await mergeFiles(repoName);
-  },
+  version: "1.1.0",
 };
