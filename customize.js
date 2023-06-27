3834a9b4ad9decf5931292fee0d6af76b050d5f9
danielalmeidajoao <d.joao@campus.fct.unl.pt>
2023-06-27 17:47:17 +0100

    Removed some unimportant files that came with pdf.js library

--- customize.js
+++ customize.js
@@ -1,3 +1,3 @@
 module.exports = {
-  version: "2.5.5",
+  version: "2.5.6",
 };
--- recordm/customUI/js/customizations2.__MERGE__.js
+++ recordm/customUI/js/customizations2.__MERGE__.js
@@ -1,5 +1,4 @@
 import("./cob/_image.js");
 
 import("./lib/pdfjs/pdf.js")
-import("./lib/pdfjs/pdf.sandbox.js")
 import("./lib/pdfjs/pdf.worker.js")
